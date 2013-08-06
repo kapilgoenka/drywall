60f4c47f32028dff7915686df0bd730fbdf9e48b
Satish Bhatti <extropy@gmail.com>
2013-08-06 14:41:18 -0700

    Added yaml config files, winston logger with graylog.

--- app.js
+++ app.js
@@ -1,23 +1,96 @@
 //dependencies
-var express = require('express'),
+var Config = require('app-server/config'),
+    log = require('app-server/log-controller').LogController.initLoggers(Config.logLevel),
+    express = require('express'),
     mongoStore = require('connect-mongo')(express),
     http = require('http'),
     path = require('path'),
     redis = require("redis"),
     RedisStore = require("connect-redis")(express),
     passport = require('passport'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    dba = require('app-server/db-accessors'),
+    cors = require('app-server/cors');
 
 //  When there's no NODE_ENV environment variable, default to development
 if (!process.env.NODE_ENV)
   process.env.NODE_ENV = 'development';
 
-//create express app
-var app = express();
+// Select custom port via command line
+var argv = require('optimist').argv;
+if ( argv.port )
+{
+  Config.port = argv.port;
+  log.debug("Using commandline port override: port = " + Config.port);
+}
+
+process.on('uncaughtException', function(err)
+{
+  var skip = ['message', 'stack', 'name', 'type', 'arguments'];
+  var text = "Uncaught exception";
+  if (err.stack)
+    text += "\n" + err.stack;
+
+  for (var p in err)
+  {
+    if (-1 !== skip.indexOf(p))
+      continue;
+    text += "\n  " + p + ": " + JSON.stringify(err[p]);
+  }
+  text += "\n";
+
+  log.error(text);
+  log.error('Killing process with pid =' + process.pid + ' due to uncaught exception');
+/*
+  setTimeout(function() {
+    process.exit(1);
+  },100);
+*/
+});
 
 var App = module.exports;
+var app = express();
 App.app = app;
 
+function initialize()
+{
+  // Configure all.
+  app.configure(configureApp);
+
+  // Config dev
+  app.configure('development', function(){
+    app.use(express.errorHandler());
+  });
+
+  //config passport
+  require('app-server/passport')(app, passport);
+
+  //route requests
+  require('app-server/routes')(app, passport);
+
+  //utilities
+  require('app-server/utilities')(app);
+
+  log.info("app.get('env') = " + app.get('env'));
+
+  dba.getDBAccessors().init(Config.mongoDBConnectURL, function(error, db)
+  {
+    app.db = db;
+
+    // Config data models.
+    require('app-server/models')(app, db);
+
+    // Listen up
+    http.createServer(app).listen(Config.port, function()
+    {
+      log.info('listening on port ' + Config.port);
+    } );
+  } );
+}
+
+initialize();
+
+/*
 //mongo uri
 app.set('mongodb-uri', process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost/drywall');
 
@@ -27,15 +100,16 @@ app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
 app.db.once('open', function () {
   console.log('mongoose open for business');
 });
+*/
 
-//config data models
-require('app-server/models')(app, mongoose);
+function configureApp()
+{
+  var redisClient = redis.createClient(Config.redis.port, Config.redis.host);
 
-var redisClient = redis.createClient();//Config.redis.port, Config.redis.host);
+  // Cross Domain (CORS)
+  app.use(cors);
 
-//config all
-app.configure(function(){
-  //settings
+  // Settings
   app.disable('x-powered-by');
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
@@ -56,26 +130,36 @@ app.configure(function(){
     ssl: true
   });
 
-  //twitter settings
-  app.set('twitter-oauth-key', 'uHL6SowhaQh6WrOVSbhtVA');
-  app.set('twitter-oauth-secret', 'LsTKjFPCSEQKGQVmdTPnU1gIGGkEA4PfMMzuGqi4');
+  // Twitter settings
+  app.set('twitter-oauth-key', Config.twitterAuth.consumerKey);
+  app.set('twitter-oauth-secret', Config.twitterAuth.consumerSecret);
 
-  //github settings
+  // Github settings
   app.set('github-oauth-key', '');
   app.set('github-oauth-secret', '');
 
-  //facebook settings
-  app.set('facebook-oauth-key', '163806877127308');//'247109212019120');
-  app.set('facebook-oauth-secret', 'f8238d22de9e81e5c71bd410006d313d');// '6ef812a00c5a4f4d0ed0ec48cee2aa6e');
+  // Facebook settings
+  app.set('facebook-oauth-key', Config.facebookAuth.consumerKey);
+  app.set('facebook-oauth-secret', Config.facebookAuth.consumerSecret);
 
-  //Singly settings
+  // Singly settings
   app.set('singly-app-id', '401fcb43bfc43038e4c6bc381c4e1530');
   app.set('singly-app-secret', '0c4ebeadda03d6b2ee0e5a4fee6b1b5b');
   app.set('singly-callback-url', 'http://localhost:' + app.get('port') + '/auth/singly/callback');
 
-  //middleware
+  // Middleware
   app.use(express.favicon(__dirname + '/public/favicon.ico'));
+
   app.use(express.logger('dev'));
+
+  app.use(express.logger({
+    format: ':date :response-time :method :url (:status)'
+  }));
+
+  app.use(express.errorHandler({
+    dumpExceptions: true
+  }));
+
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -134,32 +218,10 @@ app.configure(function(){
   app.use(app.router);
 
   //error handler
-  app.use(require('./views/http/index').http500);
+  app.use(require('views/http/index').http500);
 
   //locals
   app.locals.projectName = app.get('project-name');
   app.locals.copyrightYear = new Date().getFullYear();
   app.locals.copyrightName = app.get('company-name');
-});
-
-//config dev
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-//config passport
-require('app-server/passport')(app, passport);
-
-//route requests
-require('app-server/routes')(app, passport);
-
-//utilities
-require('app-server/utilities')(app);
-
-console.log("app.get('env') = " + app.get('env'));
-
-//listen up
-http.createServer(app).listen(app.get('port'), function()
-{
-  console.log('express server listening on port ' + app.get('port'));
-});
+}
